d01d66cedf7024363706c5d111a4f4bd0b9c9a3b
jhj <jihongjiang@outlook.com>
2017-07-09 19:22:00 +0800

    Draw trangle with webgl

---
+++ triangle/main.js
@@ -0,0 +1,122 @@
+var vertexShaderText = [
+    'precision mediump float;',
+    '',
+    'attribute vec2 vertPosition;',
+    'attribute vec3 vertColor;',
+    'varying vec3 fargColor;',
+    '',
+    'void main()',
+    '{',
+    '   fargColor = vertColor;',
+    '   gl_Position = vec4(vertPosition, 0.0, 1.0);',
+    '}'
+].join('\n');
+
+var fargmentShaderText = [
+    'precision mediump float;',
+    '',
+    'varying vec3 fargColor;',
+    'void main()',
+    '{',
+    '   gl_FragColor = vec4(fargColor, 1.0);',
+    '}'
+].join('\n');
+
+var InitDemo = function() {
+    var canvas = document.getElementById("gl-surface");
+    var gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        console.log('Webgl not supported without experimental_webgl');
+        gl = canvas.getContext('experimental-webgl');
+    }
+
+    if (!gl) {
+        alert('Your browser does not support webgl!');
+    }
+
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fargmentShaderText);
+
+    gl.compileShader(vertexShader);
+    if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS))
+    {
+        console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+        return;
+    }
+
+    gl.compileShader(fragmentShader);
+    if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS))
+    {
+        console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+        return;
+    }
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('ERROR linking program!', gl,getProgramInfoLog(program));
+        return;
+    }
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+        console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    //
+    // Create buffer
+    //
+    var triangleVertices = [
+        // X, Y      R, G, B
+        0.0, 0.5,   1.0, 0.0, 0.0,
+        -0.5, -0.5, 0.0, 1.0, 0.0,
+        0.5, -0.5,  0.0, 0.0, 1.0,
+    ];
+
+
+    var triangleVerticesBufferObj = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVerticesBufferObj);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+    var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        positionAttribLocation, // Attribute location
+        2, // Number of elements per attribute
+        gl.FLOAT, // Type of elemnets
+        gl.FALSE, // Weather or not normalize
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of single vertex
+        0 // Offset from the beginning of a single vertex to this attribute
+    );
+    gl.vertexAttribPointer(
+        colorAttribLocation, // Attribute location
+        3, // Number of elements per attribute
+        gl.FLOAT, // Type of elemnets
+        gl.FALSE, // Weather or not normalize
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of single vertex
+        2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+    );
+    gl.enableVertexAttribArray(positionAttribLocation);
+    gl.enableVertexAttribArray(colorAttribLocation);
+
+
+    //
+    // Render loop
+    //
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+};
+
+
+
+
+
